a7059ea2b9f221e0c293141351b966b260d5c41c
Ruddy Gonzalo Quispe Huanca <ruddygonzqh@gmail.com>
2023-10-12 10:56:04 -0400

    Change to documents views and template.

--- resources/views/livewire/documents/show-documents.blade.php
+++ resources/views/livewire/documents/show-documents.blade.php
@@ -38,9 +38,9 @@
                     <tr wire:key="{{ $folder->id }}">
                         <td>
                             <div class="d-flex px-2 py-1 cursor-pointer" wire:click="openFolder({{ $folder->id }})">
-                                <!-- <div>
-                                                <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">
-                                            </div> -->
+                                <div>
+                                    <i class="fa-regular fa-folder me-3"></i>
+                                </div>
                                 <div class="d-flex flex-column justify-content-center">
                                     <h6 class="mb-0 text-sm">{{ $folder->folder_name }}</h6>
                                     <p class="text-xs text-secondary mb-0">{{ $folder->description}}</p>
@@ -66,10 +66,10 @@
                     @foreach($files as $file)
                     <tr wire:key="{{ $file->id }}">
                         <td>
-                            <div class="d-flex px-2 py-1 cursor-pointer" wire:click="openFolder({{ $file->id }})">
-                                <!-- <div>
-                                                <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">
-                                            </div> -->
+                            <div class="d-flex px-2 py-1 cursor-pointer">
+                                <div>
+                                <i class="fa-solid fa-file me-3"></i>
+                                </div>
                                 <div class="d-flex flex-column justify-content-center">
                                     <h6 class="mb-0 text-sm">{{ $file->file_name }}</h6>
                                     <p class="text-xs text-secondary mb-0">{{ $file->description}}</p>
--- resources/views/livewire/users/show-roles.blade.php
+++ resources/views/livewire/users/show-roles.blade.php
@@ -7,7 +7,7 @@
                 </h6>
             </div>
             <div class="col-xl-2 col-sm-3 text-end">
-                <button type="button" class="btn btn-secondary btn-sm mb-0" data-bs-toggle="modal" data-bs-target="#createFolder">
+                <button type="button" class="btn btn-secondary btn-sm mb-0" data-bs-toggle="modal" data-bs-target="#createRole">
                     <span class="btn-inner--icon"><i class="fa fa-plus"></i></span>
                     <span class="btn-inner--text">&nbsp; New Role</span>
                 </button>
@@ -26,7 +26,6 @@
                 <thead>
                     <tr>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Rol name</th>
-                        <th class="text-secondary opacity-7"></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -52,7 +51,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="createNewPermission">Upload new file</h5>
+                    <h5 class="modal-title" id="createNewPermission">Create new permission</h5>
                     <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close" wire:click="cancel">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -62,7 +61,7 @@
                         <div class="row">
                             <div class="col">
                             <div class="form-group">
-                                    <input type="text" wire:model="permission_name" class="form-control" id="permission_name" placeholder="Folder name">
+                                    <input type="text" wire:model="permission_name" class="form-control" id="permission_name" placeholder="Permission name">
                                 </div>
                                 @error('permission_name')
                                 <span class="error" style="color:red">{{ $message }}</span>
@@ -72,7 +71,7 @@
                         <div class="row">
                             <div class="col">
                                 <div class="form-group">
-                                    <input type="text" wire:model="description" class="form-control" id="permission_description" placeholder="Folder description">
+                                    <input type="text" wire:model="description" class="form-control" id="permission_description" placeholder="Permission description">
                                 </div>
                                 @error('description')
                                 <span class="error" style="color:red">{{ $message }}</span>
@@ -83,7 +82,48 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal" wire:click="cancel">Cancel</button>
-                    <button type="submit" form="upload-file-form" class="btn bg-gradient-primary">Upload file</button>
+                    <button type="submit" form="upload-file-form" class="btn bg-gradient-primary">Create permission</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div wire:ignore.self class="modal fade" id="createRole" tabindex="-1" role="dialog" aria-labelledby="createNewRole" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createNewRole">Create new role</h5>
+                    <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close" wire:click="cancel">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body text-start">
+                    <form wire:submit="createRole" id="upload-file-form">
+                        <div class="row">
+                            <div class="col">
+                            <div class="form-group">
+                                    <input type="text" wire:model="role_name" class="form-control" id="role_name" placeholder="Role name">
+                                </div>
+                                @error('role_name')
+                                <span class="error" style="color:red">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <input type="text" wire:model="description" class="form-control" id="permission_description" placeholder="Role description">
+                                </div>
+                                @error('description')
+                                <span class="error" style="color:red">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal" wire:click="cancel">Cancel</button>
+                    <button type="submit" form="upload-file-form" class="btn bg-gradient-primary">Create role</button>
                 </div>
             </div>
         </div>
